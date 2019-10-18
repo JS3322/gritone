6eb7da983f3ed4439e31e4e15879c13a384fc233
JS KIM <i30203322@gmail.com>
2019-10-18 17:44:43 +0900

    191018_init

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+# from . < 같은 폴더 내의 models 임포트
 
 # Register your models here.
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    """ Cstom User Admin """
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
